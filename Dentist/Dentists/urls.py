b40f2d08bbc6a5be266e819eac2f41a0963e3624
Sonali <59617452+Sonali-Joshi-24@users.noreply.github.com>
2020-04-15 19:40:33 +0530

    added code for urls to link web pages

--- Dentist/Dentists/urls.py
+++ Dentist/Dentists/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hi, name='hi'),
-    path('contact.html', views.contact, name='contact'),
+    path('', views.hi, name="hi"),
+    path('hi.html', views.hi, name="hi"),
+    path('contact.html', views.contact, name="contact"),
+    path('about.html', views.about, name="about"),
+    path('service.html', views.service, name="service"),
+    path('pricing.html', views.pricing, name="pricing"),
+    
 ]
